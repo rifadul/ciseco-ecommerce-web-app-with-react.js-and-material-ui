5222bd7033750eb5702732051e44616d400491ac
siamzaagsyscom <siam@zaagsys.com>
2022-10-23 02:26:13 +0600

    feat: best selles product section complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import BoxContainer from '../components/BoxContainer'
 import Hero from '../components/home/heroSection/Hero'
+import Product from '../components/product/Product'
+import TextHeading from '../components/TextHeading'
+import { Box } from '@mui/material';
 
 const Home = () => {
     return (
         <>
+            {/* here hero section component {rf} */}
             <Hero />
+
+            {/* here hompage main info {rf} */}
+            <BoxContainer>
+                {/* here best sallers section {rf} */}
+                <Box sx={{
+                    margin: '3rem 0',
+                }}>
+                    {/* here section heading text */}
+                    <TextHeading highlightText="Best Sellers" normalText='Best selling of the month' textAlign='left' />
+                    {/* here All best selles product */}
+                    <Product />
+                </Box>
+            </BoxContainer>
         </>
     )
 }
